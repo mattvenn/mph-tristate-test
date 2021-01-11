88b36fd5dd4b3bb16305f0c3195b4a4502802454
matt venn <matt@mattvenn.net>
2021-01-11 18:28:28 +0100

    added power pins

--- test.v
+++ test.v
@@ -5,9 +5,9 @@
 `define UNIT_DELAY #1
 `define USE_POWER_PINS
 
-`include "libs.ref/sky130_fd_io/verilog/sky130_fd_io.v"
-`include "libs.ref/sky130_fd_io/verilog/sky130_ef_io.v"
-`include "libs.ref/sky130_fd_io/verilog/sky130_ef_io__gpiov2_pad_wrapped.v"
+//`include "libs.ref/sky130_fd_io/verilog/sky130_fd_io.v"
+//`include "libs.ref/sky130_fd_io/verilog/sky130_ef_io.v"
+//`include "libs.ref/sky130_fd_io/verilog/sky130_ef_io__gpiov2_pad_wrapped.v"
 
 `include "libs.ref/sky130_fd_sc_hd/verilog/primitives.v"
 `include "libs.ref/sky130_fd_sc_hd/verilog/sky130_fd_sc_hd.v"
@@ -32,7 +32,15 @@ module test (
     // hand placed
     // example taken from https://github.com/shalan/DFFRAM/blob/main/Handcrafted/Models/DFFRAMBB.v#L205
     // https://antmicro-skywater-pdk-docs.readthedocs.io/en/86-cell_cross_index/contents/libraries/sky130_fd_sc_hd/cells/ebufn/README.html
-    sky130_fd_sc_hd__ebufn_4 tri_buf ( .A( tri_in ), .Z(tri_out), .TE_B(!enable) );
+    sky130_fd_sc_hd__ebufn_4 tri_buf (
+        .A( tri_in ),
+        .Z(tri_out),
+        .TE_B(!enable),
+        .VPWR(1'b1),
+        .VGND(1'b0),
+        .VPB(1'b1),
+        .VNB(1'b0)
+        );
 
     // inferred
 //    assign tri_out = enable ? tri_in : 1'bz;
