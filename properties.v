34063145f01dfa179b9954ac2ea91934c5589c2b
Matt Venn <matt@mattvenn.net>
2021-01-14 14:58:12 +0100

    add properties

--- properties.v
+++ properties.v
@@ -1,3 +1,8 @@
 always @(*)
-    if(reset)
-        assert(tri_out == 8'bz);
+    if(!active) begin
+        assert(wbs_ack_o    != buf_wbs_ack_o);
+        assert(wbs_dat_o    == 32'bz);
+        assert(la_data_out  == 32'bz);
+        assert(io_out       == `MPRJ_IO_PADS'bz);
+        assert(io_oeb       == `MPRJ_IO_PADS'bz);
+    end
