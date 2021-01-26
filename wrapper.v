69ece0c0071a37171743e96082bac4db69c1f28d
Matt Venn <matt@mattvenn.net>
2021-01-26 18:03:34 +0100

    power pins

--- wrapper.v
+++ wrapper.v
@@ -4,6 +4,16 @@
 `endif
 module wrapper (
     // interface as user_proj_example.v
+`ifdef USE_POWER_PINS
+    inout vdda1,        // User area 1 3.3V supply
+    inout vdda2,        // User area 2 3.3V supply
+    inout vssa1,        // User area 1 analog ground
+    inout vssa2,        // User area 2 analog ground
+    inout vccd1,        // User area 1 1.8V supply
+    inout vccd2,        // User area 2 1.8v supply
+    inout vssd1,        // User area 1 digital ground
+    inout vssd2,        // User area 2 digital ground
+`endif
     input wire wb_clk_i,
     input wire wb_rst_i,
     input wire wbs_stb_i,
